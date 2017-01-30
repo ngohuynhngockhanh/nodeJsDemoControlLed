c00201d0da6465827b4efa0212a7899fac5cf8fb
ngohuynhngockhanh <ngohuynhngockhanh@gmail.com>
2017-01-30 21:46:44 +0700

    chinh lai tab

--- index.js
+++ index.js
@@ -4,23 +4,23 @@ var io = require('socket.io')(server);
 
 server.listen(8000); //lắng nghe ở port 8000
 
-app.get('/', function (req, res) { //tạo webserver khi truy nhập đường dẫn "/".
-  res.sendfile(__dirname + '/index.html'); // thì mở nội dung ở file index.html lên
+app.get('/', function(req, res) { //tạo webserver khi truy nhập đường dẫn "/".
+    res.sendfile(__dirname + '/index.html'); // thì mở nội dung ở file index.html lên
 });
 
-io.on('connection', function (socket) {//sau khi client kết nối tới server 
-	//khi server nhận được yêu cầu "bật" từ client
-  socket.on('bật', function () {
-		console.log("bật")
-  });
-  //khi server nhận được yêu cầu "tắt" từ client
-  socket.on('tắt', function () {
-		console.log("tắt")
-  });
-  //khi server nhận được yêu cầu "nhấp nháy" từ client
-  socket.on('nhấp nháy', function () {
-		console.log("nhấp nháy")
-  });
+io.on('connection', function(socket) { //sau khi client kết nối tới server 
+    //khi server nhận được yêu cầu "bật" từ client
+    socket.on('bật', function() {
+        console.log("bật")
+    });
+    //khi server nhận được yêu cầu "tắt" từ client
+    socket.on('tắt', function() {
+        console.log("tắt")
+    });
+    //khi server nhận được yêu cầu "nhấp nháy" từ client
+    socket.on('nhấp nháy', function() {
+        console.log("nhấp nháy")
+    });
 });
 
 
